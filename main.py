ce88f32f7b44a9515e3f450dcf68d5b13d87366b
TroyDownend <troy.noel.id@gmail.com>
2024-07-05 12:35:55 -0700

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello world")
+def read_frankenstein(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        print file_contents
+def main():
+    read_frankenstein
