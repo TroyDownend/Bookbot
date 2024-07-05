9aff89b5c1d65cef15feb4ad21869c99cb01fb6c
TroyDownend <troy.noel.id@gmail.com>
2024-07-05 12:57:51 -0700

    lots of code updates to main.py

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-def read_frankenstein(path_to_file):
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        print file_contents
 def main():
-    read_frankenstein
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
 
-main.py
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_char_dict(text):
+    letter_dict = {}
+    letters = [letter for letter in text]
+    for letter in letters:
+        if letter in letter_dict:
+            letter_dict[letter] += 1
+        else:
+            letter_dict[letter] = 1
+    return letter_dict
+
+def create_dict_report(dictionary):
+    for key in dictionary:
+        value = key
+        print(f"'{key}' was character was found {value} times")
+
+main()
